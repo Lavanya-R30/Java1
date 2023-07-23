8f257b301242672c7cb95efe73e3d2fd18dce834
Lavanya-R30 <lavanyarl.xworkz@gmail.com>
2023-07-23 22:15:14 +0530

    message

---
+++ java basics/constructor/chaining/Helmate.java
@@ -0,0 +1,28 @@
+public class Helmate
+e{
+	double price;
+	char  helmateSize;
+	static String helmateBrand;
+	static String manufacturingState;
+	
+	//method uses instance variables
+	public void helmateInstanceVariables(){
+		System.out.printf("Price: %f \nhelmateSize: %c", price,helmateSize);
+	}
+	
+	//method uses static variables
+	static void staticVariableMethod(){
+		System.out.printf("Brand: %s \nmanufacturingState: %s",helmateBrand,manufacturingState);
+	}
+	
+	Helmate(double price,char helmateSize){
+		this.price = price;
+		this.helmateSize = helmateSize;
+	}
+	
+	//static block
+	static{
+		helmateBrand = "Vega";
+		manufacturingState = "Karnataka";
+	}
+}
