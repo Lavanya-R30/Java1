5dd5779b63cb82b9fd0095c01219f0f370ddf312
Lavanya-R30 <lavanyarl.xworkz@gmail.com>
2023-07-31 21:45:35 +0530

    sports&players

---
+++ 31-07-2023/Sport/src/com/xworkz/sport/app/Player.java
@@ -0,0 +1,28 @@
+package com.xworkz.sport.app;
+
+public class Player {
+	public String name;
+	public int exp;
+	public HomeTown town;
+	public Building building;
+	
+	public Player(String name,int exp, HomeTown town, Building building) {
+		
+		this.name=name;
+		this.exp=exp;
+		this.town=town;
+		this.building=building;
+		
+	}
+	
+	public void printInfo() {
+		
+		System.out.println("Player Name: "+name);
+		System.out.println("Player Experience: "+exp);
+		System.out.println("***********************************");
+		town.printInfo();
+		System.out.println("***********************************");
+		building.printInfo();
+		
+}
+}
