adf89fdb395af5d4b12c3a1012eaee0325f72454
Lavanya-R30 <lavanyarl.xworkz@gmail.com>
2023-08-06 21:42:49 +0530

    chaning

---
+++ SuperChaning/Diamond/src/com/xworkz/diamond/app/Diamond.java
@@ -0,0 +1,19 @@
+package com.xworkz.diamond.app;
+
+public class Diamond {
+
+	public String type;
+	public String color;
+	public double price;
+	public float sizeInMm;
+	
+	public Daimond(String type,String color,double price,float sizeInMm) {
+		
+		this.type=type;
+		this.color=color;
+		this.price=price;
+		this.sizeInMm=sizeInMm;
+		
+		System.out.println("Invoking String,String,double,float in Daimond");
+	}
+}
---
+++ SuperChaning/Diamond/src/com/xworkz/diamond/boot/DiamondRunner.java
@@ -0,0 +1,25 @@
+package com.xworkz.diamond.boot;
+
+import com.xworkz.diamond.app.FancyColoredDiamond;
+
+public class DiamondRunner {
+	public static void main(String[] args) {
+		System.out.println("Running main in DaimondRunner");
+		
+		FancyColoredDaimond daimond=new FancyColoredDaimond();
+		System.out.println("Daimond type"+daimond.type);
+		System.out.println("Daimond price"+daimond.price);
+		System.out.println("Daimond Color"+daimond.color);
+		System.out.println("Daimond Size In mm"+daimond.sizeInMm);
+		
+		System.out.println("********************");
+		
+		FancyColoredDaimond daimond1=new FancyColoredDaimond("Mined daimonds","White",86000,5.2f);
+		System.out.println("Daimond type"+daimond1.type);
+		System.out.println("Daimond price"+daimond1.price);
+		System.out.println("Daimond Color"+daimond1.color);
+		System.out.println("Daimond Size In mm"+daimond1.sizeInMm);
+
+	}
+
+}
