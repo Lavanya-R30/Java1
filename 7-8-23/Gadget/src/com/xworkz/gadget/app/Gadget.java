137a85b1ab38cceea8da773517c315ea061cf6dd
Lavanya-R30 <lavanyarl.xworkz@gmail.com>
2023-08-08 10:16:14 +0530

    methodoverload

--- 7-8-23/Gadget/src/com/xworkz/gadget/app/Gadget.java
+++ 7-8-23/Gadget/src/com/xworkz/gadget/app/Gadget.java
@@ -1,5 +1,27 @@
 package com.xworkz.gadget.app;
 
 public class Gadget {
+	public String type;
+	public double price;
 
+	public void item() {
+		System.out.println("invoking item of no-arg..");
+		item("Mobile");
+		item(14999);
+	}
+
+	public void item(String type) {
+		System.out.println("invoking item of String..");
+		System.out.println("Type :" + type);
+	}
+
+	public void item(double price) {
+		System.out.println("invoking item of double...");
+		System.out.println("Price :" + price);
+	}
+
+	public void item(String type, double price) {
+		item(type);
+		item(price);
+}
 }
