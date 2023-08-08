30eb4cc85f04fd78fcc5faa5205484f7add42197
Lavanya-R30 <140000373+Lavanya-R30@users.noreply.github.com>
2023-08-08 09:01:12 +0530

    Update GadgetRunner.java

--- 7-8-23/Gadget/src/com/xworkz/gadget/boot/GadgetRunner.java
+++ 7-8-23/Gadget/src/com/xworkz/gadget/boot/GadgetRunner.java
@@ -1,9 +1,20 @@
+
 package com.xworkz.gadget.boot;
+
+
+import com.xworkz.gadget.app.Gadget;
 
 public class GadgetRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+			Gadget product =new Gadget();
+			product.item();
+			System.out.println("\n");
+			product.item("Laptop",48000);
+
+		
 
 	}
 
