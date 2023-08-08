594602b0bcebd378fb69f8e274d9daf25da0635d
Lavanya-R30 <140000373+Lavanya-R30@users.noreply.github.com>
2023-08-08 09:11:48 +0530

    Update UmberallaRunner.java

--- 7-8-23/Umberalla/src/com/xworkz/umberalla/boot/UmberallaRunner.java
+++ 7-8-23/Umberalla/src/com/xworkz/umberalla/boot/UmberallaRunner.java
@@ -1,10 +1,15 @@
 package com.xworkz.umberalla.boot;
 
+import com.xworkz.umberalla.app.Umberalla;
+
 public class UmberallaRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Umbrella drop=new Umbrella();
+		drop.rain();
+		System.out.println("\n");
+		drop.rain("Black",250);
 	}
 
 }
