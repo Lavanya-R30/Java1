7ac21464e237efdcd510ff1b2130195326b16cfe
Lavanya-R30 <lavanyarl.xworkz@gmail.com>
2023-08-16 22:17:48 +0530

    objecttask

---
+++ Object/Flag/src/com/xworkz/object/boot/FlagRunner.java
@@ -0,0 +1,23 @@
+package com.xworkz.object.boot;
+
+import com.xworkz.object.app.Flag;
+
+public class FlagRunner {
+
+	public static void main(String[] args) {
+
+		System.out.println("invoking main in FlagRunner ");
+
+		Flag flag = new Flag("Orange White Green", 5, true);
+		System.out.println(flag);
+
+		Flag flag1 = new Flag("White", 10, false);
+		System.out.println(flag1);
+
+		Flag flag2 = new Flag("Green", 3, false);
+		System.out.println(flag2);
+
+	
+	}
+
+}
